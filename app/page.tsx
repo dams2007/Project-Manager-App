1ad7a6ec50e2c5c2f359a86e5c58ac606750f2bd
Damien Bigot <damien.bigot@gmail.com>
2024-06-06 14:28:56 +0900

    remove uncessary loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Navbar } from "@/app/components//navbar";
 import ProjectList from "@/app/components//ProjectList";
-import Loading from "@/app/components//Loading";
 
 export default function Home() {
 	return (
 		<main className="bg-main-bg-color items-center lg:max-w-8xl h-screen">
 			<Navbar />
-			<Suspense fallback={<Loading />}>
-				<ProjectList />
-			</Suspense>
+			<ProjectList />
 		</main>
 	);
 }
