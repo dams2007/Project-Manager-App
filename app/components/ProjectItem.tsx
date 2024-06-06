fcf352ad87d58cafea47011ecfb5c61abd3b035d
Damien Bigot <damien.bigot@gmail.com>
2024-06-06 12:35:25 +0900

    convert table into a list to set link on a li

--- app/components/ProjectItem.tsx
+++ app/components/ProjectItem.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import databaseIcon from "@/public/database-icon.svg";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/app/components/button";
 import { ProjectData } from "@/app/types/ProjectData";
 import { ConvertDate } from "@/app/utils/dateConverter";
 import { ProjectStatus } from "@/app/types/ProjectStatus";
@@ -16,35 +15,32 @@ const ProjectItem = ({ project }: ProjectItemProps) => {
 	const statusColorClass = statusColorMap[project.status as ProjectStatus];
 	const statusDisplayText = statusDisplayMap[project.status as ProjectStatus];
 	return (
-		<tr className="bg-white border-b light:bg-gray-800 light:border-gray-700 hover:bg-gray-50 light:hover:bg-gray-600 h-14">
-			<td
-				scope="row"
-				className=" rounded-l-lg rounded-b-lg px-6 py-4 font-medium text-primary-color whitespace-nowrap light:text-white"
+		<Link href={`/editProject/${project.id}`}>
+			<li
+				key={project.id}
+				className="bg-white border-b light:bg-gray-800 light:border-gray-700 hover:bg-gray-50 light:hover:bg-gray-600 h-20 flex items-center m-3 rounded-lg justify-between"
 			>
-				<div className="flex items-center gap-3">
+				<div className="px-6 py-4 font-medium text-primary-color whitespace-nowrap light:text-white flex items-center gap-3 w-1/3">
 					<Image priority src={databaseIcon} alt="Database Icon" />
 					{project.title}
 				</div>
-			</td>
-			<td scope="row" className="text-primary-color px-6 py-4">
-				<div className="sm:flex sm:flex-col sm:items-start">
-					<div className="mt-1 flex items-center gap-x-1.5">
-						<div
-							className={`flex-none rounded-full ${statusColorClass} p-1`}
-						></div>
-						<p className="text-primary-color">{statusDisplayText}</p>
+				<div className="flex items-center justify-end w-1/2 gap-6">
+					<div className="text-primary-color px-6 py-4 w-1/3">
+						<div className="sm:flex sm:flex-col sm:items-start">
+							<div className="flex items-center gap-x-1.5">
+								<div
+									className={`flex-none rounded-full ${statusColorClass} p-1`}
+								></div>
+								<p className="text-primary-color">{statusDisplayText}</p>
+							</div>
+						</div>
+					</div>
+					<div className="text-primary-color px-6 py-4 w-1/3">
+						{ConvertDate(project.createdAt as string)}
 					</div>
 				</div>
-			</td>
-			<td scope="row" className="text-primary-color  rounded-r-lg px-6 py-4">
-				{ConvertDate(project.createdAt as string)}
-			</td>
-			<td className="text-primary-color rounded-r-lg px-6 py-4">
-				<Button className="self-center mr-12 rounded-md bg-secondary-color px-3.5 py-2.5 text-sm font-normal text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800">
-					<Link href={`/editProject/${project.id}`}>Edit</Link>
-				</Button>
-			</td>
-		</tr>
+			</li>
+		</Link>
 	);
 };
 
--- app/components/ProjectList.tsx
+++ app/components/ProjectList.tsx
@@ -45,28 +45,20 @@ const ProjectList = () => {
 	}
 
 	return (
-		<div className="w-full relative overflow-x-auto ">
+		<div className="w-full relative overflow-x-auto">
 			<div className="mx-12 mt-6 mb-1">
-				<table className="w-full text-sm text-left rtl:text-right border-spacing-y-2 border-separate">
-					<thead className="text-primary-color h-2">
-						<tr>
-							<th scope="col" className="px-6 text-sm font-normal">
-								Title
-							</th>
-							<th scope="col" className="px-6 text-sm font-normal">
-								Status
-							</th>
-							<th scope="col" className="px-6 text-sm font-normal">
-								Date Created
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						{projects.map((project) => (
-							<ProjectItem key={project.id} project={project} />
-						))}
-					</tbody>
-				</table>
+				<ul className="w-full text-sm text-left rtl:text-right border-spacing-y-2 border-separate list-none">
+					<li className="text-primary-color h-8 flex items-center justify-between">
+						<div className="px-6 text-sm font-normal w-1/3">Title</div>
+						<div className="flex items-center justify-end w-1/2 gap-6 ">
+							<div className="px-6 text-sm font-normal w-1/3">Status</div>
+							<div className="px-6 text-sm font-normal w-1/3">Date Created</div>
+						</div>
+					</li>
+					{projects.map((project) => (
+						<ProjectItem key={project.id} project={project} />
+					))}
+				</ul>
 			</div>
 		</div>
 	);
